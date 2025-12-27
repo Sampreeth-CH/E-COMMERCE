328e26f5e37ad10a1d7a98df4e070107bf1984c9
Sampreeth C H <sampreethchsampreethch@gmail.com>
2025-12-27 09:39:24 +0530

    added .env to backend folder

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -29,41 +29,6 @@ export const Login = (email, password) => async (dispatch) => {
   }
 }
 
-// export const Register = (name, email, password) => async (dispatch) => {
-//   try {
-//     dispatch({
-//       type: 'USER_REGISTER_REQUEST',
-//     })
-//     const config = {
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     }
-//     const { data } = await axios.post(
-//       '/api/users',
-//       { name, email, password },
-//       config
-//     )
-//     dispatch({
-//       type: 'USER_REGISTER_SUCCESS',
-//       payload: data,
-//     })
-//     dispatch({
-//       type: 'USER_LOGIN_SUCCESS',
-//       payload: data,
-//     })
-//     localStorage.setItem('userInfo', JSON.stringify(data))
-//   } catch (error) {
-//     dispatch({
-//       type: 'USER_REGISTER_FAIL',
-//       payload:
-//         error.response && error.response.data.message
-//           ? error.response.data.message
-//           : error.message,
-//     })
-//   }
-// }
-
 export const Register = (name, email, password) => async (dispatch) => {
   try {
     dispatch(USER_REGISTER_REQUEST())
