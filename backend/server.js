6e7926fb6291777239c1f667325367186538c2e7
Sampreeth C H <sampreethchsampreethch@gmail.com>
2026-01-07 14:08:06 +0530

    feat: Update CORS configuration to allow multiple origins

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://skymart-shop.vercel.app'],
 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
